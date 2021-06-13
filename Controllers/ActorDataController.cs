668c373d5dd8a9fb0dab03884069ecd28f5174e5
Haroon Shaffiulla <haroonshaffy@gmail.com>
2021-06-13 05:09:14 -0400

    Added CRUD functionality to Actors

---
+++ Controllers/ActorDataController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using UpcomingMoviesApplication.Models;
+
+namespace UpcomingMoviesApplication.Controllers
+{
+    public class ActorDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/ActorData/ListActors
+        [HttpGet]
+        public IQueryable<Actor> ListActors()
+        {
+            return db.Actors;
+        }
+
+        // GET: api/ActorData/FindActor/5
+        [ResponseType(typeof(Actor))]
+        [HttpGet]
+        public IHttpActionResult FindActor(int id)
+        {
+            Actor actor = db.Actors.Find(id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(actor);
+        }
+
+        // POST: api/ActorData/UpdateActor/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateActor(int id, Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != actor.ActorID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(actor).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ActorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ActorData/AddActor
+        [ResponseType(typeof(Actor))]
+        [HttpPost]
+        public IHttpActionResult AddActor(Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Actors.Add(actor);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = actor.ActorID }, actor);
+        }
+
+        // POST: api/ActorData/DeleteActor/5
+        [ResponseType(typeof(Actor))]
+        [HttpPost]
+        public IHttpActionResult DeleteActor(int id)
+        {
+            Actor actor = db.Actors.Find(id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            db.Actors.Remove(actor);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ActorExists(int id)
+        {
+            return db.Actors.Count(e => e.ActorID == id) > 0;
+        }
+    }
+}
